28466ab7f35c2421956f99197d61171c7ae0883b
Chris Sedlmayr <chris@sedlmayr.co.uk>
2015-05-13 21:11:12 +0100

    Added tick handler, and function to update time hour
    Caters for 24h option.
    
    We now have the hour being output and updated on the window

--- src/pebble-hour-wf.c
+++ src/pebble-hour-wf.c
@@ -3,23 +3,47 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[] = "00";
+
+  // Write the current hours and minutes into the buffer
+  if (clock_is_24h_style()) {
+    strftime(buffer, sizeof("00"), "%H", tick_time);
+  } else {
+    strftime(buffer, sizeof("00"), "%I", tick_time);
+  }
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changes) {
+  update_time();
+}
+
 static void main_window_load(Window *window) {
   // Create time TextLayer
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "00");
-
   // Improve the layout to be more like a watchface
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+
+  update_time();
 }
 
 static void main_window_unload(Window *window) {
-
+  // Destroy the text layer
+  text_layer_destroy(s_time_layer);
 }
 
 static void init() {
@@ -34,6 +58,9 @@ static void init() {
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
+
+  // Register with TickTimerService, only update when the minute changes
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit() {
