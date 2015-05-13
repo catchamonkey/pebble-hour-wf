da9f01e16ae91c7a76d3f4d9076044c2ff6fbadb
Chris Sedlmayr <chris@sedlmayr.co.uk>
2015-05-13 19:40:32 +0100

    Changes font to be Roboto bold subset 49, and only outputting hour now

--- src/pebble-hour-wf.c
+++ src/pebble-hour-wf.c
@@ -8,10 +8,10 @@ static void main_window_load(Window *window) {
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_text(s_time_layer, "00");
 
   // Improve the layout to be more like a watchface
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
