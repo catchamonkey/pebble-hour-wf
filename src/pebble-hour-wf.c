806ad51ca9c799e378b081e0f023277c73d014c7
Chris Sedlmayr <chris@sedlmayr.co.uk>
2015-05-18 16:59:53 +0100

    All working!
     * Background colour now updates once per unit (minute, second when debug)
     * Adds debug mode flag.
     * text color now changes based on the bg colour brightness
     * BG size now works all the way through, it was a rounding issue as I was dealing with int so the decimal was discarded

--- src/pebble-hour-wf.c
+++ src/pebble-hour-wf.c
@@ -1,8 +1,35 @@
 #include <pebble.h>
 
+#define DEBUG true
+
 static Window *s_main_window;
 static Layer *s_canvas_layer;
 static TextLayer *s_time_layer;
+
+static GColor get_color_from_minute(uint8_t minute) {
+  uint8_t argb = GColorBlackARGB8 + minute + 1;
+
+  if (argb >= GColorDarkGrayARGB8) { argb++; }
+  if (argb >= GColorLightGrayARGB8) { argb++; }
+
+  return (GColor8){ .argb = argb };
+}
+
+/**
+ * Gets colour to use for font, based on the best choice matching the given bgcolor
+ * http://stackoverflow.com/questions/946544/good-text-foreground-color-for-a-given-background-color/946734#946734
+ *
+ * @param  GColor bgcolor
+ * @return GColor
+ */
+static GColor get_fgcolor_for_bgcolor(GColor bgcolor) {
+  uint8_t r, g, b;
+  r = (bgcolor.argb & 0b110000) >> 4;
+  g = (bgcolor.argb & 0b001100) >> 2;
+  b = bgcolor.argb & 0b000011;
+  uint16_t brightness = r*299 + g*587 + b*114;
+  return brightness < 1750 ? GColorWhite : GColorBlack;
+}
 
 static void update_time() {
   // Get a tm structure
@@ -30,83 +57,45 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changes) {
   // either 0-100% BG fill, or a radial 0-360 degrees, same way a minute hand does, but filling colour
 }
 
+static GColor8 bgcolor;
+static GColor8 fgcolor;
+
 static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
   GRect bounds = layer_get_bounds(this_layer);
   // Get the center of the screen (non full-screen)
-  // GPoint center = GPoint(bounds.size.w / 2, (bounds.size.h / 2));
+  GPoint center = GPoint(bounds.size.w / 2, (bounds.size.h / 2));
 
   time_t temp = time(NULL);
   struct tm *tick_time = localtime(&temp);
 
-  int minute = tick_time->tm_min;
+  uint8_t color_index = DEBUG ? tick_time->tm_sec : tick_time->tm_min;
 
-  if (minute == 0) { graphics_context_set_fill_color(ctx, GColorBlack); }
-  if (minute == 1) { graphics_context_set_fill_color(ctx, GColorOxfordBlue); }
-  if (minute == 2) { graphics_context_set_fill_color(ctx, GColorDukeBlue); }
-  if (minute == 3) { graphics_context_set_fill_color(ctx, GColorBlue); }
-  if (minute == 4) { graphics_context_set_fill_color(ctx, GColorDarkGreen); }
-  if (minute == 5) { graphics_context_set_fill_color(ctx, GColorMidnightGreen); }
-  if (minute == 6) { graphics_context_set_fill_color(ctx, GColorCobaltBlue); }
-  if (minute == 7) { graphics_context_set_fill_color(ctx, GColorBlueMoon); }
-  if (minute == 8) { graphics_context_set_fill_color(ctx, GColorIslamicGreen); }
-  if (minute == 9) { graphics_context_set_fill_color(ctx, GColorJaegerGreen); }
-  if (minute == 10) { graphics_context_set_fill_color(ctx, GColorTiffanyBlue); }
-  if (minute == 11) { graphics_context_set_fill_color(ctx, GColorVividCerulean); }
-  if (minute == 12) { graphics_context_set_fill_color(ctx, GColorGreen); }
-  if (minute == 13) { graphics_context_set_fill_color(ctx, GColorMalachite); }
-  if (minute == 14) { graphics_context_set_fill_color(ctx, GColorMediumSpringGreen); }
-  if (minute == 15) { graphics_context_set_fill_color(ctx, GColorCyan); }
-  if (minute == 16) { graphics_context_set_fill_color(ctx, GColorBulgarianRose); }
-  if (minute == 17) { graphics_context_set_fill_color(ctx, GColorImperialPurple); }
-  if (minute == 18) { graphics_context_set_fill_color(ctx, GColorIndigo); }
-  if (minute == 19) { graphics_context_set_fill_color(ctx, GColorElectricUltramarine); }
-  if (minute == 20) { graphics_context_set_fill_color(ctx, GColorArmyGreen); }
-  if (minute == 21) { graphics_context_set_fill_color(ctx, GColorDarkGray); }
-  if (minute == 22) { graphics_context_set_fill_color(ctx, GColorLiberty); }
-  if (minute == 23) { graphics_context_set_fill_color(ctx, GColorVeryLightBlue); }
-  if (minute == 24) { graphics_context_set_fill_color(ctx, GColorKellyGreen); }
-  if (minute == 25) { graphics_context_set_fill_color(ctx, GColorMayGreen); }
-  if (minute == 26) { graphics_context_set_fill_color(ctx, GColorCadetBlue); }
-  if (minute == 27) { graphics_context_set_fill_color(ctx, GColorPictonBlue); }
-  if (minute == 28) { graphics_context_set_fill_color(ctx, GColorBrightGreen); }
-  if (minute == 29) { graphics_context_set_fill_color(ctx, GColorScreaminGreen); }
-  if (minute == 30) { graphics_context_set_fill_color(ctx, GColorMediumAquamarine); }
-  if (minute == 31) { graphics_context_set_fill_color(ctx, GColorElectricBlue); }
-  if (minute == 32) { graphics_context_set_fill_color(ctx, GColorDarkCandyAppleRed); }
-  if (minute == 33) { graphics_context_set_fill_color(ctx, GColorJazzberryJam); }
-  if (minute == 34) { graphics_context_set_fill_color(ctx, GColorPurple); }
-  if (minute == 35) { graphics_context_set_fill_color(ctx, GColorVividViolet); }
-  if (minute == 36) { graphics_context_set_fill_color(ctx, GColorWindsorTan); }
-  if (minute == 37) { graphics_context_set_fill_color(ctx, GColorRoseVale); }
-  if (minute == 38) { graphics_context_set_fill_color(ctx, GColorPurpureus); }
-  if (minute == 39) { graphics_context_set_fill_color(ctx, GColorLavenderIndigo); }
-  if (minute == 40) { graphics_context_set_fill_color(ctx, GColorLimerick); }
-  if (minute == 41) { graphics_context_set_fill_color(ctx, GColorBrass); }
-  if (minute == 42) { graphics_context_set_fill_color(ctx, GColorLightGray); }
-  if (minute == 43) { graphics_context_set_fill_color(ctx, GColorBabyBlueEyes); }
-  if (minute == 44) { graphics_context_set_fill_color(ctx, GColorSpringBud); }
-  if (minute == 45) { graphics_context_set_fill_color(ctx, GColorInchworm); }
-  if (minute == 46) { graphics_context_set_fill_color(ctx, GColorMintGreen); }
-  if (minute == 47) { graphics_context_set_fill_color(ctx, GColorCeleste); }
-  if (minute == 48) { graphics_context_set_fill_color(ctx, GColorRed); }
-  if (minute == 49) { graphics_context_set_fill_color(ctx, GColorFolly); }
-  if (minute == 50) { graphics_context_set_fill_color(ctx, GColorFashionMagenta); }
-  if (minute == 51) { graphics_context_set_fill_color(ctx, GColorMagenta); }
-  if (minute == 52) { graphics_context_set_fill_color(ctx, GColorOrange); }
-  if (minute == 53) { graphics_context_set_fill_color(ctx, GColorSunsetOrange); }
-  if (minute == 54) { graphics_context_set_fill_color(ctx, GColorBrilliantRose); }
-  if (minute == 55) { graphics_context_set_fill_color(ctx, GColorShockingPink); }
-  if (minute == 56) { graphics_context_set_fill_color(ctx, GColorChromeYellow); }
-  if (minute == 57) { graphics_context_set_fill_color(ctx, GColorRajah); }
-  if (minute == 58) { graphics_context_set_fill_color(ctx, GColorMelon); }
-  if (minute == 59) { graphics_context_set_fill_color(ctx, GColorRichBrilliantLavender); }
+  bgcolor = get_color_from_minute(color_index);
+  graphics_context_set_fill_color(ctx, bgcolor);
+
+
+  fgcolor = get_fgcolor_for_bgcolor(bgcolor);
+  text_layer_set_text_color(s_time_layer, fgcolor);
 
   // Draw a circle
-  // graphics_context_set_fill_color(ctx, GColorFromRGB(0, 0, minute*3));
-  // graphics_context_set_fill_color(ctx, colour);
-  // graphics_fill_circle(ctx, center, 70);
-  graphics_fill_rect(ctx, GRect(0, 0, bounds.size.w/60*minute, bounds.size.h), 0, GCornerNone);
-  // GRect(xstart, ystart, xsize, ysize)
+  // int radius = (bounds.size.w/2/60)*color_index;
+  // graphics_fill_circle(ctx, center, radius);
+
+  // Or draw a rect
+  int width = (bounds.size.w/60.0) * color_index;
+  graphics_fill_rect(ctx, GRect(0, 0, width, bounds.size.h), 0, GCornerNone);
+
+  // Or to make the rect grow from the centre.
+  // x offset should be half the screen, minus, half the width of the rect
+  // int x_offset = (bounds.size.w/2) - (width/2);
+
+
+  // Or leave rect as full width, then adjust x offset to move into view until fully in
+  // int x_offset = (0 - bounds.size.w) + (bounds.size.w / 60) * color_index;
+  // int x_offset = (0 - bounds.size.w) + (bounds.size.w);
+  // x_offset = ceil(x_offset);
+
+  // graphics_fill_rect(ctx, GRect(x_offset, 0, bounds.size.w, bounds.size.h), 0, GCornerNone);
 }
 
 static void main_window_load(Window *window) {
@@ -117,6 +106,8 @@ static void main_window_load(Window *window) {
   // Create Layer
   s_canvas_layer = layer_create(GRect(0, 0, window_bounds.size.w, window_bounds.size.h));
   layer_add_child(window_layer, s_canvas_layer);
+
+  window_set_background_color(s_main_window, GColorDarkGray);
 
   // Set the update_proc
   layer_set_update_proc(s_canvas_layer, canvas_update_proc);
@@ -153,8 +144,9 @@ static void init() {
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
 
-  // Register with TickTimerService, only update when the minute changes
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  // Register with TickTimerService, only update when the minute changes (unless debug is enabled)
+  TimeUnits update_freq = DEBUG ? SECOND_UNIT : MINUTE_UNIT;
+  tick_timer_service_subscribe(update_freq, tick_handler);
 }
 
 static void deinit() {
